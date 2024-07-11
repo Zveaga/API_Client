ab694f0264785d399f8c740c9e50215b976fd32a
Zveaga <98222905+Zveaga@users.noreply.github.com>
2024-07-11 13:40:17 +0200

    wip

--- script.js
+++ script.js
@@ -6,25 +6,27 @@ console.log('START');
 
 // fs.readFile()
 const apiURL = 'https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest?symbol=BTC';
-const apiKey = '';
+const apiKey = '17921b1a-07b1-4264-8260-3547e4d9af36';
 
 const requestOptions = {
 	method: 'GET',
 	headers: {
-		'Authorizarion': `Bearer ${apiKey}`,
+		'X-CMC_PRO_API_KEY:': apiKey,
+		// 'Authorization': `Bearer ${apiKey}`,
+		'Accept': 'application/json'
 	},
 };
 
 fetch(apiURL, requestOptions)
-	.then(function response() {
+	.then(response => {
 		if (!response.ok) {
 			throw new Error('Response error!');
 		}
 		return (response.json());
 	})
-	.then(function data() {
+	.then(data => {
 		console.log(data);
 	})
-	.catch(function error() {
+	.catch(error => {
 		console.error('Error: ', error);
-	});
+});
